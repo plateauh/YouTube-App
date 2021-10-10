7dc252e6f989230d9673162a612966d55961dce3
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-10 10:22:25 +0300

    thanks god

--- app/src/main/java/com/example/youtubeapp/MainActivity.kt
+++ app/src/main/java/com/example/youtubeapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.youtubeapp
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        if (activeNetwork?.isConnectedOrConnecting == true){
+
+        }
+        else {
+
+        }
     }
 }
